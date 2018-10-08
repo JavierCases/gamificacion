5cd03d6ed5afb17cb10eea7bf439b003cfad65cb
Alumno <JavierCases>
2018-10-08 09:56:29 +0200

    He creado el metodo remoto /getNombreInstancia soy javier cases

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Juego) {
+/**
+ * Busca un nombre
+ * @param {Function(Error, string)} callback
+ */
+
+Juego.prototype.getNameInstancia = function(callback) {
+  var nombre;
+  var juego = this;
+  // TODO
+  callback(null, juego.nombre);
+};
+
 
 };
